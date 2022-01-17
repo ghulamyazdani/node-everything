63d5e4a8187baef7a3ca3d7badba4d7d82444705
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2022-01-18 01:00:24 +0530

    Done with this branch

--- tryme/server.js
+++ tryme/server.js
@@ -1,9 +1,11 @@
+import {svgret} from "./svgret.js"
 import express from 'express';
  const app = express();
 
-  app.get('/chalra=:color', (req, res) =>(
-      res.json({color:req.params.color})
+  app.get('/date', (req, res) =>(
+      res.send(svgret(req.query))
   ))
+  const port = process.env.PORT || 3000;
   app.listen('3000',() =>{
-      console.log("listening on http://localhost:3000")
+      console.log(`listening on http://localhost:${port}`)
   })
